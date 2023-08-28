ddcc0bee2eb71c583571cda5cb239f92ceadbbe3
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-28 16:57:58 +0200

    ex1

---
+++ ex1/ex1/Data/APIContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ex1.Models;
+
+namespace ex1.Data
+{
+
+    public partial class APIContext : DbContext
+    {
+        public APIContext(DbContextOptions<APIContext> options)
+        : base(options)
+        {
+        }
+
+        public virtual DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_0900_ai_ci");
+
+            modelBuilder.Entity<Cliente>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+                entity.ToTable("usuarios");
+
+                entity.Property(e => e.Nombre)
+                        .HasMaxLength(250)
+                        .HasColumnName("nombre");
+
+                entity.Property(e => e.Apellido)
+                        .HasMaxLength(250)
+                        .HasColumnName("apellido");
+
+                entity.Property(e => e.Direccion)
+                        .HasMaxLength(250)
+                        .HasColumnName("direccion");
+
+                entity.Property(e => e.Dni).HasColumnName("dni");
+
+                entity.Property(e => e.Fecha).HasColumnName("fecha");
+
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
+
+
---
+++ ex1/ex1/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿namespace ex1.Models
+{
+
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        public string Apellido { get; set; } = null!;
+
+        public string Direccion { get; set; } = null!;
+
+        public string Dni { get; set; } = null!;
+
+        public DateOnly Fecha { get; set; }
+    }
+}
+
